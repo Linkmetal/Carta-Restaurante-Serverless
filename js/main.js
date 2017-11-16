5de02fe86250fb77832d3c931907635742037f6e
Linkmetal <linkmetal18@gmail.com>
2017-11-16 10:02:27 +0000

    Deshabilitado el ESLint (no me gustan las reglas de AirBnB)
    Añadida la funcion para elegir un color

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
+// global variables
+let selectedColor;
+//
 function init() {
-    let colors = ["blue", "red", "pink", "yellow"];
-    let buttonIcons = ["fa fa-plus-square-o", "fa fa-times", "fa fa-strikethrough", "fa  fa-check-square-o", "fa fa-square-o", "fa fa-sort"];
-    let colorsDiv = document.createElement("div");
+    const colors = ["blue", "red", "pink", "yellow"];
+    const buttonIcons = ["fa fa-plus-square-o", "fa fa-times", "fa fa-strikethrough", "fa  fa-check-square-o", "fa fa-square-o", "fa fa-sort"];
+    const colorsDiv = document.createElement("div");
     colorsDiv.id = "colorsDiv";
-    
-    for(let i = 0; i < colors.length; i++) {
-        let color = document.createElement("div");
+
+    for (let i = 0; i < colors.length; i++) {
+        const color = document.createElement("div");
         color.className = "color";
         color.style.backgroundColor = colors[i];
         colorsDiv.appendChild(color);
+        if (i === 0) {
+            selectedColor = color;
+            selectedColor.className += " selectedColor";
+        } 
     }
 
-    
     let node = document.createElement("input");
     node.type = "text";
     node.className = "textBox";
@@ -22,14 +28,24 @@ function init() {
     node = document.createElement("div");
     node.id = "buttonsDiv";
 
-    for(let i = 0; i < buttonIcons.length; i++){
-        let icon = document.createElement("i");
+    for (let i = 0; i < buttonIcons.length; i++) {
+        const icon = document.createElement("i");
         icon.className = buttonIcons[i];
         icon.className += " button";
         node.appendChild(icon);
     }
 
     document.getElementById("mainContainer").appendChild(node);
-    
+
+    initListeners();
+}
+
+function initListeners() {
+    let colors = [].slice.call(document.querySelectorAll(".color"));
+    colors.map(e => e.addEventListener("click", function(){
+        selectedColor.className = "color";
+        selectedColor = this;
+        selectedColor.className = "color selectedColor";
+    }, false));
 }
 
